c6dafea0bee0e025b897dc9863285dbdfb9491a7
Jeff-Anderson-C <80008336+Jeff-Anderson-C@users.noreply.github.com>
2022-08-11 15:34:35 -0500

    images

--- sotan_collab/settings.py
+++ sotan_collab/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
